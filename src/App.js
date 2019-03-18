bf01342bdbb7b2ae4979ebe97634fe41adcdbb96
Bryant <cabrera.bryant@gmail.com>
2019-03-17 22:25:22 -0700

    Creates Nav.js component and imports it into App.js.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Route, Switch, withRouter } from 'react-router-dom';
 import Header from './components/Header/Header';
+import Nav from './components/Nav/Nav';
 import Main from './components/Main/Main';
 import './App.css';
 
@@ -9,6 +10,7 @@ class App extends Component {
         return (
         <div className="App">
             <Header />
+            <Nav />
             <Switch>
                 <Route
                     exact
---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { withRouter, NavLink } from 'react-router-dom';
+import '../../App.css';
+
+class Nav extends Component {
+    render() {
+        return (
+            <nav className="nav">
+                
+            </nav>
+        )
+    }
+}
+
+export default withRouter(Nav);
