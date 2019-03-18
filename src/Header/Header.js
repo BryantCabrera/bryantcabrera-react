eea8b86483ee33e5b66509559e97cb86b6f85934
Bryant <cabrera.bryant@gmail.com>
2019-03-17 21:14:50 -0700

    Creates Header.js component.

---
+++ src/Header/Header.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { withRouter, NavLink } from 'react-router-dom';
+import '../../App.css';
+
+class Header extends Component {
+    render() {
+        return (
+            <header className="header">
+                {/* <NavLink exact to="/" className="header__logo--link">
+                    <img className="header__logo" src="https://i.imgur.com/Y40XSUQ.png" alt="Bryant Cabrera Logo" title="Bryant Cabrera Logo"></img></NavLink>
+                <ul className="header__links">
+                    <li className="header__links--item"><a href="https://github.com/BryantCabrera/gameplay-react" target="_blank" rel="noopener noreferrer"><img className="header__links--item--github" src="/imgs/github.svg" alt="GitHub" title="GitHub"></img></a></li>
+                    <li className="header__links--item"><a href="https://www.linkedin.com/in/bryantcabrera/" target="_blank" rel="noopener noreferrer"><img className="header__links--item--linkedin" src="/imgs/linkedin.png" alt="LinkedIn" title="LinkedIn"></img></a></li>
+                </ul> */}
+            </header>
+        )
+    }
+}
+
+export default withRouter(Header);
