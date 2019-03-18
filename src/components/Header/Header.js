2fcedbe9d206237ad380e37b310b0b42a8746381
Bryant <cabrera.bryant@gmail.com>
2019-03-18 01:24:16 -0700

    Updates header icons and links.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,6 +3,17 @@ import { withRouter, NavLink } from 'react-router-dom';
 import '../../App.css';
 
 class Header extends Component {
+    state = {
+        email: ''
+    }
+
+    toggleEmail = () => {
+        const email = this.state.email === '' ? 'cabrera.bryant@gmail.com' : '';
+        this.setState({
+            email: email
+        });
+    }
+
     render() {
         return (
             <header className="header">
@@ -10,10 +21,24 @@ class Header extends Component {
                     <img src="/imgs/bryantcabrera_logo.svg" alt="Bryant Cabrera's Logo" title="Bryant Cabrera's Logo" className="logo--img"></img>
                 </NavLink>
                 <ul className="contact__list">
-                    <li className="contact__list--item"><a className="contact__list--link" href="#about">About</a></li>
-                    <li className="contact__list--item"><a className="contact__list--link"  href="#projects">Projects</a></li>
-                    <li className="contact__list--item"><a className="contact__list--link"  href="#resume">Resume</a></li>
-                    <li className="contact__list--item"><a className="contact__list--link"  href="#contact">Contact</a></li>
+                    <li className="contact__list--item">
+                        <span className="contact__list__email">{this.state.email}</span>
+                    </li>
+                    <li className="contact__list--item">
+                        <a className="contact__list--link" href="#about" onClick={this.toggleEmail}>
+                            <img className="contact__list--img" src="/imgs/gmail.png" alt="Bryant's Email" title="Bryant's Email"></img>
+                        </a>
+                    </li>
+                    <li className="contact__list--item">
+                        <a className="contact__list--link" href="https://github.com/BryantCabrera" target="_blank" rel="noopener noreferrer">
+                            <img className="contact__list--img" src="/imgs/github.png" alt="Bryant's GitHub" title="Bryant's GitHub"></img>
+                        </a>
+                    </li>
+                    <li className="contact__list--item">
+                        <a className="contact__list--link" href="https://www.linkedin.com/in/bryantcabrera/" target="_blank" rel="noopener noreferrer">
+                            <img className="contact__list--img" src="/imgs/linkedin--header.png" alt="Bryant's LinkedIn" title="Bryant's LinkedIn"></img>
+                        </a>
+                    </li>
                 </ul>
             </header>
         )
