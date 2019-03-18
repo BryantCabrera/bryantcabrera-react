07b7771ab6248d5adecbebbc8f31b7306bab6d95
Bryant <cabrera.bryant@gmail.com>
2019-03-17 22:42:32 -0700

    Styles about link in Nav.js component.

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -6,7 +6,9 @@ class Nav extends Component {
     render() {
         return (
             <nav className="nav">
-                
+                <NavLink exact to="/" className="nav__about">About</NavLink>
+                <NavLink exact to="/" className="nav__projects">Projects</NavLink>
+                <NavLink exact to="/" className="nav__resume">Resume</NavLink>
             </nav>
         )
     }
