502c2d2cd79c7ad0bb9a1e300563b502a518540c
Bryant <cabrera.bryant@gmail.com>
2019-03-19 14:15:00 -0700

    Updates

---
+++ precache-manifest.7ec0ab853d537448a697ce9644a7d2a1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "cee79994f82ff3900a63",
+    "url": "/static/js/main.29d51227.chunk.js"
+  },
+  {
+    "revision": "558834f033b0e0279cb7",
+    "url": "/static/js/2.29d0fb1b.chunk.js"
+  },
+  {
+    "revision": "cee79994f82ff3900a63",
+    "url": "/static/css/main.f808c2e1.chunk.css"
+  },
+  {
+    "revision": "d1fc798d65a3625bb9826b3a1ceea561",
+    "url": "/index.html"
+  }
+];
